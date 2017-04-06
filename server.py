22f69265b7c6e5bdced5cc2eab372634c80269dd
Toby Liu <ybot1122@gmail.com>
2017-04-06 10:49:56 -0700

    python server

--- server.py
+++ server.py
@@ -1,11 +1,49 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+from AppChains import AppChains
+
 class UsageExample(object):
-    token = 'fMoBS4UvQ2EYxYSv49Zdd3yRE5kw5QpGsxJQxvU2JiC8P2XSuTm9aXDfJueVhSZox1faIus8NBxfrQ8Ed6QaYw'
+    secret = 'fMoBS4UvQ2EYxYSv49Zdd3yRE5kw5QpGsxJQxvU2JiC8P2XSuTm9aXDfJueVhSZox1faIus8NBxfrQ8Ed6QaYw'
     url = 'api.sequencing.com'
+    token = ''
 
     def __init__(self):
         self.chains = AppChains(self.token, self.url)
+        #print(self.get_public_beacon_test())
         print(self.get_raw_report_test())
+        self.get_report_test()
         self.get_report_batch_test()
+
+
+    def get_public_beacon_test(self):
+        beacon_result = self.chains.getPublicBeacon(1, 2, 'A')
+        return beacon_result
+
+    def get_raw_report_test(self):
+        chains_raw_result = self.chains.getRawReport(
+            'StartApp', 'Chain12', '227680')
+        return chains_raw_result
+
+    def get_report_test(self):
+        chains_result = self.chains.getReport(
+            'StartApp', 'Chain87', '227680')
+        if chains_result.isSucceeded():
+            print('Request has succeeded')
+        else:
+            print('Request has failed')
+        for r in chains_result.getResults():
+            file_type = r.getValue().getType()
+            v = r.getValue()
+            if file_type == 'TEXT':
+                print('-> text result type {} = {}'.format(
+                    r.getName(), v.getData()))
+            elif file_type == 'FILE':
+                print(' -> file result type {} = {}'.format(
+                    r.getName(), v.getUrl()
+                ))
+                v.saveTo('/tmp')
+
     def get_report_batch_test(self):
         chains_results = self.chains.getReportBatch(
             'StartAppBatch', {'Chain85': '227680', 'Chain88': '227680'})
@@ -25,4 +63,5 @@ class UsageExample(object):
                         r.getName(), v.getUrl()
                     ))
                     v.saveTo('/tmp')
+
 UsageExample()
