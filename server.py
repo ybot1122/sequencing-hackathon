02ed4792034ad82d0cea4b1f03d3bae20a5a04a3
Toby Liu <ybot1122@gmail.com>
2017-04-06 10:37:47 -0700

    init commit with example python sever and react app'

---
+++ server.py
@@ -0,0 +1,28 @@
+class UsageExample(object):
+    token = 'fMoBS4UvQ2EYxYSv49Zdd3yRE5kw5QpGsxJQxvU2JiC8P2XSuTm9aXDfJueVhSZox1faIus8NBxfrQ8Ed6QaYw'
+    url = 'api.sequencing.com'
+
+    def __init__(self):
+        self.chains = AppChains(self.token, self.url)
+        print(self.get_raw_report_test())
+        self.get_report_batch_test()
+    def get_report_batch_test(self):
+        chains_results = self.chains.getReportBatch(
+            'StartAppBatch', {'Chain85': '227680', 'Chain88': '227680'})
+        for chains_result in chains_results:
+            if chains_results[chains_result].isSucceeded():
+                print('Request has succeeded')
+            else:
+                print('Request has failed')
+            for r in chains_results[chains_result].getResults():
+                file_type = r.getValue().getType()
+                v = r.getValue()
+                if file_type == 'TEXT':
+                    print('-> text result type {} = {}'.format(
+                        r.getName(), v.getData()))
+                elif file_type == 'FILE':
+                    print(' -> file result type {} = {}'.format(
+                        r.getName(), v.getUrl()
+                    ))
+                    v.saveTo('/tmp')
+UsageExample()
